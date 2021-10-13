b0df14786e3581b63b7e41fc687e6ba4bd5e029f
btarcahn <bach.tran@employmenthero.com>
2021-10-13 22:57:24 +0700

    Add nil.component.tsx

---
+++ src/components/nil.component.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {Container, Image} from "react-bootstrap";
+
+const ADMIN_EMAIL = 'bach.tran@employmenthero.com';
+
+type Props = {
+
+};
+
+type State = {
+    message: string,
+    triggered_by: string
+};
+
+export const NilComponent: React.FC<Props> = () => (
+    <Container style={{ minHeight: '20vh', maxHeight: '50vh' }}>
+        <h3>Oops! Something happened!</h3>
+        <Container style={{ maxHeight: '75%', maxWidth: '75%' }}>
+            <Image src="https://img.freepik.com/free-vector/404-error-with-character-error-design-template-website_114341-24.jpg?size=626&ext=jpg"
+                   fluid alt="Error 404 image" />
+        </Container>
+        You are seeing this because we've messed up somewhere... <br/>
+        Please contact { ADMIN_EMAIL }for support.
+    </Container>
+    );
